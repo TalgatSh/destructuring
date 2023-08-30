fd3f317766dacaadd7be08d76d5681b850ba2c28
Talgat Shukataev <tshukataev@mail.ru>
2023-08-30 20:51:30 +0500

    first commit

---
+++ src/app/app.js
@@ -0,0 +1,29 @@
+const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
+};
+
+export default function getSuperAttack({ special }) {
+  const result = [];
+  special.forEach((element) => (element?.description ? result.push(element) : result.push({ ...element, description: 'Описание не доступно' })));
+
+  return result;
+}
